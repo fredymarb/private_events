67e608c20b3377fa7a8b64fb5eb914232aaaa646
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-04-16 19:01:21 +0000

    add necessary association between user and event models
    desc:
      add a one to many relationship between
      user[one] and event[many] models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: "User"
+
   validates :name, presence: true
   validates :date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_16_184722) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_16_185412) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_16_184722) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "creator_id"
 end
