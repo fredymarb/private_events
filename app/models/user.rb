0c30e5790034b41c8f3ec3fc84f2c0930d372d94
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-04-16 19:01:21 +0000

    add necessary association between user and event models
    desc:
      add a one to many relationship between
      user[one] and event[many] models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :created_events, class_name: "Event", foreign_key: "creator_id"
+
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
