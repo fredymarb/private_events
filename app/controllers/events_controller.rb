6239b0bdb296917ee9c2e5b75d085700deb4229e
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-04-17 01:35:05 +0000

    feat: update EventsController
    desc:
      update the EventsController with necessary actions
      to allow users create a new event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,26 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      flash[:notice] = "Event created successfully."
+      redirect_to event_path(@event)
+    else
+      flash.now[:alert] = "Failed to create event."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :name, :date ])
+  end
 end
