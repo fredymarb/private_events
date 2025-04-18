f2bbac718105f98c52062540646368141333443e
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-04-18 20:21:47 +0000

    add controller for attendance model

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,16 @@
+class AttendancesController < ApplicationController
+  def create
+    event = Event.find(params[:event_id])
+    Attendance.create!(attendee: current_user, attended_event: event)
+
+    flash[:notice] = "You have been added to this event"
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    attendance = Attendance.find_by(attendee: current_user, attended_event: event)
+    attendance.destroy
+
+    flash[:notice] = "You have been removed from this event"
+  end
+end
