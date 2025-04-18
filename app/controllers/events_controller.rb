f7c2e1e31e168ff9295bc7558f29f4ffdeb0c758
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-04-18 08:36:51 +0000

    fix events controller to work with form_with helper method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,22 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+
   def index
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def new
-    @event = current_user.created_events.build
+    @user = current_user
+    @event = @user.created_events.build
   end
 
   def create
-    @event = current_user.created_events.build(event_params)
+    @user = current_user
+    @event = @user.created_events.build(event_params)
 
     if @event.save
       flash[:notice] = "Event created successfully."
